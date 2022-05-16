28df1b43a26bb93b28bb4f28b25b6ac49c9da4a1
Andre Stendahl <andre.stendahl@liftforward.com>
2022-05-16 13:16:16 -0700

    finishing cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,29 @@ class SessionsController < ApplicationController
   end
 
   def login
+
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      #puts "YOU SIGNED IN SUCCESS WOOHOO"
+
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
     #lookup user by username in db
     #see if passwprds match
   end
 
   def create
-
     @user = User.find_by(username: params[:username])
-
     if @user && @user.authenticate(params[:password])
-      #set new auth token first
-      #return a cookie to user's browser
-      #value of cookie is equal to users authorization token
+      #set new auth token first - DONE
+      @user.set_auth_token
+      #return a cookie to user's browser - DONE?
+      #value of cookie is equal to users authorization token - DONE?
+      cookies[:authorization_token] = @user.authorization_token
+
       redirect_to '/welcome'
     else
       redirect_to '/login'
